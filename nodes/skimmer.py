4cefe850d5e1e8c4639e4b95c2a17d1d3296482a
Jeremiah Via <jeremiah.via@gmail.com>
2011-12-11 21:27:01 +0000

    Changing message type. Will likely create a new one.

--- nodes/skimmer.py
+++ nodes/skimmer.py
@@ -1,35 +1,30 @@
 #!/usr/bin/env python
 import roslib; roslib.load_manifest('ros_aucom_bridge')
 import rospy
+from geometry_msgs.msg import Twist
+from ros_aucom_bridge.msg import TopicSignature
 
-from ros_aucom_bridge.msg import Signature
-
-pub = rospy.Publisher('skimmed_topics', Signature)
-ignore_list = ['rosgraph_msgs/Log', 'rosgraph_msgs/Clock', 'ros_aucom_bridge/Signature']
-time = 0.0
+pub = rospy.Publisher('skimmed_topics', TopicSignature)
 
 def skimmer():
     rospy.init_node('skimmer')
-    global time
-    time = rospy.get_rostime()
-
     # get topics from list parameter
-    if rospy.has_param('~topics'):
-        print 'Reading in topics list'
-        topics = rospy.get_param("~topics")
-        for line in topics.splitlines():
-            topic_name, topic_type = line.split()
-            rospy.loginfo('Topic name: %s, Topic type: %s', topic_name, topic_type)
-            di(topic_type)
-    else:
-        rospy.logerror('No topics list found, subscribing to all running topics. THIS WILL LIKELY NOT WORK')
-        # for topic_name, topic_type in rospy.client.get_published_topics():
-        #     if (not ignore(topic_type)):
-        #         print 'Making callback for {0} of type {1}'.format(topic_name, topic_type.split('/')[1])
-        # rospy.Subscriber(topic_name,
-        #                  eval(class_name(topic_type)),
-        #                  make_subsriber(topic_name, topic_type))
-    rospy.spin()
+    if not rospy.has_param('~topics'):
+        rospy.logerror('No topics list found. Ensure the topics parameter has a string of topics.')
+        exit(1)
+
+    topics = rospy.get_param("~topics")
+    for line in topics.splitlines():
+        topic_name, topic_type = line.split()
+        rospy.loginfo('Topic name: %s, Topic type: %s', topic_name, topic_type)
+        di(topic_type)
+        class_name = topic_type.split('/')[1]
+
+        print class_name
+        rospy.Subscriber(topic_name,
+                         eval(class_name),
+                         make_subsriber(topic_name, topic_type))
+        rospy.spin()
 
 
 def di(topic_type):
@@ -45,30 +40,19 @@ def di(topic_type):
         except ImportError:
             rospy.logerr('Could not import %s. Skipping', msg)
 
-
 def make_subsriber(topic_name, topic_type):
     return lambda msg: topic_publisher(msg, topic_name, topic_type)
 
-def dynamic_import(topic_type):
-    print ''
-
-def class_name(topic_type):
-    return topic_type.split('/')[1]
-
 
 def topic_publisher(msg, topic_name, topic_publisher):
-    try:
-        print '{0:10} :: {1:<15} {2:<15}'.format(rostime_to_ms(), topic_name, topic_publisher)
-    except AttributeError:
-        print 'ERROR: {0} {1}'.format(topic_name, topic_publisher)
+    pub.publish(rospy.get_rostime(), topic_name, topic_type)
+    rospy.loginfo('%s', '{0:10} :: {1:<15} {2:<15}'.format(rospy.get_rostime(), topic_name, topic_type))
+
 
 def rostime_to_ms():
     return (rospy.get_rostime().secs - time.secs) * 1000 + (rospy.get_rostime().nsecs - time.nsecs) / 1000000
-# return rospy.get_time() - time
 
 def ignore(topic_type):
-    """
-    """
     for ignored in ignore_list:
         if (ignored == topic_type): return True
     return False
